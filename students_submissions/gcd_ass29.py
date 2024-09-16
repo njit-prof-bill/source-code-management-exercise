1faa8d60911cc7dcc59951711097e19a24eea614
ayman <ass29@njit.edu>
2024-09-16 19:45:39 -0400

    Implemented GCD function

---
+++ students_submissions/gcd_ass29.py
@@ -0,0 +1,9 @@
+def gcd(a: int, b: int) -> int:
+    """
+    Calculate the greatest common divisor (GCD) of two integers a and b
+    using the Euclidean algorithm.
+    """
+    while b:
+        a, b = b , a % b
+
+    return a
