eacbc61fb0b9cb1076f0314b5e9074196f76fae5
WokFriedE <47959663+WokFriedE@users.noreply.github.com>
2024-09-16 19:49:50 -0400

    Added docstring

--- students_submissions/gcd_WokFriedE.py
+++ students_submissions/gcd_WokFriedE.py
@@ -1,4 +1,5 @@
 def gcd(a: int, b: int) -> int:
+    """Calculate the greatest common divisor (GCD) of two integers a and b using the Euclidean algorithm."""
     while b:
         a, b = b, a % b
     return a
