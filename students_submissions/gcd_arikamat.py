03676cb4672f1458cd2e7181ebbcbb1c7a86774c
Ari Kamat <arijkamat@gmail.com>
2024-09-16 20:01:41 -0400

    Add test cases

--- students_submissions/gcd_arikamat.py
+++ students_submissions/gcd_arikamat.py
@@ -4,3 +4,7 @@ def gcd(a: int, b: int) -> int:
     if a==0:
         return b
     return gcd(b%a, a)
+# Test cases 
+print(gcd(54, 24)) # Expected output: 6 
+print(gcd(48, 18)) # Expected output: 6 
+print(gcd(101, 10)) # Expected output: 1
