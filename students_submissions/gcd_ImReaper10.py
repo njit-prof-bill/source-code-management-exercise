b96dcefa888b28b8a02da7a5c16c36a9c75746fa
ImReaper10 <husainawan10@gmail.com>
2024-09-16 20:30:31 -0400

    Implemented GCD function

---
+++ students_submissions/gcd_ImReaper10.py
@@ -0,0 +1,10 @@
+def gcd(a: int, b: int) -> int:
+    while b:
+        a, b = b, a % b
+    return a
+
+print(gcd(54, 24))
+# Expected output: 6
+print(gcd(48, 18))
+# Expected output: 6
+print(gcd(101, 10)) # Expected output: 1
