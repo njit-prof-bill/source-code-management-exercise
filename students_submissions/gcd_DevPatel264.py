b76c3d351647a0a61bca664498dbcc548da38535
Dev Patel <dpatel8475@hccc.edu>
2024-09-20 15:05:36 -0400

    Adding Changes

--- students_submissions/gcd_DevPatel264.py
+++ students_submissions/gcd_DevPatel264.py
@@ -3,7 +3,7 @@ def gcd(a: int, b: int) -> int:
         a, b = b, a % b
     return a
 
-# Test cases
+# Test cases:
 print(gcd(120, 35))  # Expected output: 5 
 print(gcd(81, 27))   # Expected output: 27 
 print(gcd(56, 98))   # Expected output: 14
