215df462e03295ad8e4ef43872ec7649aa7a8384
Justin Nguyen <justinnguyen@justins-air.resource.campus.njit.edu>
2024-09-20 20:21:39 -0400

    implemnted GCD function

--- gcd_jnguyen657.py
+++ gcd_jnguyen657.py
@@ -1,8 +1,8 @@
 def gcd(a: int, b: int) -> int:
     """Calculate the greatest common divisor (GCD) of two integers a and b using the Euclidean algorithm."""
-    while b != 0:
-        a, b = b, a % b
-    return abs(a)
+    while b: 
+        a, b = b, a % b 
+    return a
 
 def main():
     # Test cases
