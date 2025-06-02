ac4f983055fdc0c64864d22e4d907a9ec59821c5
paperTigerToo <zacharykochemail@gmail.com>
2025-06-01 21:26:20 -0400

    Update gcd_zlk.py
    Implemented GCD function

--- students_submissions/gcd_zlk.py
+++ students_submissions/gcd_zlk.py
@@ -21,7 +21,7 @@ def gcd(a: int, b: int) -> int:
             
     
 
-# Test case
+# Test cases
 print(gcd(54, 24))  # Expected output: 6
 print(gcd(48, 18))  # Expected output: 6
 print(gcd(101, 10))  # Expected output: 1
